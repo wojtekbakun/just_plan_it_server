0b7b498c838873f56e7ff13e8e32b376b21bc572
wojtekbakun <wojciechzsee@gmail.com>
2024-06-11 19:28:28 +0200

    added gemini api

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+console.log(" de: ", process.env.GEMINI_API_KEY);
+
+// The Gemini 1.5 models are versatile and work with most use cases
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+  const prompt = "Write me 5 random words"
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
