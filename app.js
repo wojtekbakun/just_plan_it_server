cb5ebb27bc8cd51e5823c14dde981cdbe5d0fcf4
wojtekbakun <wojciechzsee@gmail.com>
2024-07-07 17:27:13 +0200

    added dotenv and updated google calendar api code

--- app.js
+++ app.js
@@ -1,9 +1,57 @@
 const http = require('http');
 const url = require('url');
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const e = require('express');
+
+const {google} = require('googleapis');
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI,
+);
+
+// generate a url that asks permissions for Google Calendar scope
+const scopes = [
+  'https://www.googleapis.com/auth/calendar.events'
+];
+
+const calendar = google.calendar({
+  version: 'v3',
+  auth: process.env.GOOGLE_API_KEY,
+});
+
+// async function sendEvent() {
+//   const event = {
+//     'summary': "eventData.summary",
+//     'description': "eventData.description",
+//     'start': {
+//       'dateTime': "2024-07-07T09:00:00-02:00",
+//       'timeZone': 'Europe/Warsaw',
+//     },
+//     'end': {
+//       'dateTime': "2024-07-07T09:30:00-02:00",
+//       'timeZone': 'Europe/Warsaw',
+//     },
+//   }
+
+//   calendarz.events.insert({
+//   auth: auth,
+//   calendarId: 'primary',
+//   resource: event,
+// }, function(err, event) {
+//   if (err) {
+//     console.log('There was an error contacting the Calendar service: ' + err);
+//     return;
+//   }
+//  console.log('Event created: %s', event.link);
+//    });
+// }
+
 
 // Access your API key as an environment variable 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
@@ -21,12 +69,75 @@ app.get('/', (req, res) => {
   res.send('Welcome in Calend.ai!!');
 });
 
-// app.get('/generate', async (req, res) => {
-//   const data = req.data;
-//   const prompt = req.query.prompt;
-//   const result = await run(prompt);
-//   res.send(result);
-// });
+app.get('/google', async (req, res) => {
+  // Odczytujemy tablicę eventów z req.body.events
+
+  //const events = req.body.events;
+
+  // // Przechodzimy przez każdy event i wysyłamy go do kalendarza
+  // for (const eventData of events) {
+  //   // Zmieniamy nazwy kluczy na te oczekiwane przez sendEvent
+  //   await sendEvent(authorize(), eventData);
+  // }
+
+  const authUrl = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  access_type: 'offline',
+
+  // If you only need one scope, you can pass it as a string
+  scope: scopes
+});
+  res.redirect(authUrl);
+});
+
+//ZA KAZDYM RAZEM JEST INNY TOKEN ?!?
+
+app.get('/google/redirect', async (req, res) => {
+  // Odczytujemy kod autoryzacyjny z req.query.code
+  const code = req.query.code;
+  console.log(req.query);
+  
+  // This will provide an object with the access_token and refresh_token.
+  // Save these somewhere safe so they can be used at a later time.
+  const {tokens} = await oauth2Client.getToken(code)
+  oauth2Client.setCredentials(tokens);
+  
+  res.send('You are now connected to Google Calendar');
+});
+
+app.post('/send-event', async (req, res) => {
+
+  summary = req.body["title"];
+  description = req.body["description"];
+
+  
+
+const event = {
+  'summary': `title:${summary}`,
+  'description': description,
+  'start': {
+    'dateTime': '2024-07-07T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+  'end': {
+    'dateTime': '2024-07-07T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles',
+  },
+};
+
+calendar.events.insert({
+  auth: oauth2Client,
+  calendarId: 'primary',
+  resource: event,
+}, function(err, event) {
+  if (err) {
+    console.log('There was an error contacting the Calendar service: ' + err);
+    return;
+  }
+  res.send('Event created');
+  console.log('Event created: %s', event.htmlLink);
+});
+});
 
 app.post('/generate', async (req, res) => {
   const data = req.body;
@@ -42,48 +153,31 @@ app.listen(PORT, () => {
 
 async function run(userInput) {
 
-  const responseSchema = 
-  {
-  "type": "object",
-  "properties": {
-    "event-title": {
-      "type": "string"
-    },
-    "event-description": {
-      "type": "string"
-    },
-    "start-date": {
-      "type": "string"
-    },
-    "end-date": {
-      "type": "string"
-    },
-    "resource-link": {
-      "type": "string"
-    }
-  },
-  "required": [
-    "event-title",
-    "event-description",
-    "start-date",
-    "end-date",
-    "resource-link"
-  ]
-}
-  const prompt = `Jesteś ekspertem odnośnie zarządzania czasem oraz planowania zadań.
-  Twoją specjalnością jest planowanie długoterminowe. Podam ci rzecz, którą chcę opanować i czas jaki chcę na to poświęcić,
-  a ty na podstawie mojego planu rozpisz mi czynności, które mam wykonać lub tematy, które muszę opanować,
-  aby zrealizować cel. Podziel je na drobne zadania, które będę sukcesywnie wykonywać.
+  const prompt = `
+  You are an expert in time management and task planning. Your specialty is long-term planning.
+  I will provide you with a skill I want to master and the time I want to dedicate to it.
+  Based on my plan, you will outline tasks and topics for me to master.
+  Divide them into small tasks that I will complete gradually.
+  Today is 06.07.2024.
 
-  Chcę opanować: ${userInput}
+Focus on:
 
-  Twoja odpowiedź niech będzie w formacie JSON, zawierającym pola:
-  events:
-  - title: tytuł zadania
-  - description: opis zadania
-  - start-date: data rozpoczęcia zadania
-  - end-date: data zakończenia zadania
-  - resource-link: link do zasobu, który pomoże mi w realizacji zadania
+  - Providing a realistic timeframe for completing a given task (2 hours max but should be less than 1 hour).
+  -	Prioritizing tasks based on their importance and deadline.
+	-	Spreading tasks over a longer period (e.g., several months), considering time for rest and unforeseen circumstances.
+	-	Suggesting optimal deadlines for future tasks to be realistic and achievable.
+	-	Adjusting the plan in case of changing priorities or new tasks.
+
+I want to master: ${userInput}
+
+Your response should be in JSON format, containing the fields:
+
+	events:
+	•	title: task title
+	•	description: task description
+	•	startDate: task start date and time (YYYY-MM-DDTHH:mm:ssZ)
+	•	endDate: task end date and time (YYYY-MM-DDTHH:mm:ssZ)
+	•	resourceLink: link to a resource that will help me complete the task
   `;
 
   const model = genAI.getGenerativeModel({
